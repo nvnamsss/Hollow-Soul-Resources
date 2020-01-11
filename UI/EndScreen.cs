0ae80e12e27a35cb4e27364b419da5f04af33f67
Nhựt <nhutcaucui@gmail.com>
2020-01-11 15:16:56 +0700

    no message

---
+++ UI/EndScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(returnToMenu());
+    }
+
+    IEnumerator returnToMenu()
+    {
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene("MainMenu");
+
+    }
+}
